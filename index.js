ca722924d79bb7344e5233a15834e13a3c88d20a
Alex Savolainen <alex.savolainen@aalto.fi>
2023-01-06 22:13:28 +0200

    3.19

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ const errorHandler = (error, request, response, next) => {
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
     }
+    else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+      }
 
     next(error)
 }
@@ -71,7 +74,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     if (!body.name || !body.number) {
         response.status(400).json({ error: 'name or number missing' })
@@ -84,7 +87,9 @@ app.post('/api/persons', (request, response) => {
         person.save().then(result => {
             response.json(result)
         })
+        .catch(error => next(error))
     }
+    
 })
 
 app.use(errorHandler)
