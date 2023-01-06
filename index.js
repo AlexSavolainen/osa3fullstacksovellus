7d7f914245229deceadec3dddee9500bcd86d133
Alex Savolainen <alex.savolainen@aalto.fi>
2023-01-06 23:16:09 +0200

    .

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Person = require('./models/Person')
+const Person = require('./models/person')
 
 
 const app = express()
